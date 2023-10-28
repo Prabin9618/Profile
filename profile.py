4741d7e44be3a8c0dbb6519ca432d5ba65a363dc
Prabin <prabindash135@gmail.com>
2023-10-28 23:13:14 +0530

    Updated with boxes

--- profile.py
+++ profile.py
@@ -2,15 +2,32 @@ import streamlit as st
 
 st.set_page_config(layout='wide')
 
+with open('style.css') as f:
+    css = f.read()
+
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+
 st.markdown("""<style>img{border-radius: 50%;}</style>""", unsafe_allow_html=True)
 
 # Resume PDF file
 with open("PD Resume.pdf", "rb") as pdf_file:
     PDFbyte = pdf_file.read()
 
-col1, col2, col3, col4= st.columns([0.25,0.2,0.3,0.25])
+col1, col2, col3= st.columns([0.2,0.55,0.25])
 
 with col1:
+    st.image("Photo.jpg", width=180)
+
+with col2:
+    st.title("PRABIN DASH")
+    st.markdown("Data Scientist")
+    st.markdown("Bhubaneswar, India")
+    st.download_button(label="Download Resume",
+                    data=PDFbyte,
+                    file_name="Resume_PrabinDash.pdf",
+                    mime='application/octet-stream')
+
+with col3:
     collink1, collink2 = st.columns(spec=[0.1,0.9])
     with collink1:
         st.markdown("")
@@ -25,92 +42,75 @@ with col1:
         st.markdown("prabindash135@gmail.com")
         st.markdown("+91 9778841064")
 
-with col2:
-    st.image("Photo.jpg", width=180)
+st.markdown("####")
 
-with col3:
-    st.title("PRABIN DASH")
-    st.markdown("Data Scientist")
-    st.markdown("Bhubaneswar, India")
-    st.download_button(label="Download Resume",
-                    data=PDFbyte,
-                    file_name="Resume_PrabinDash.pdf",
-                    mime='application/octet-stream')
-
-with col4:
-    st.markdown("<h3 style='text-align: center;'>EDUCATION</h3>", unsafe_allow_html=True)
-    st.markdown("<div style='text-align: center;'>B.Tech (2014-2018)</div>", unsafe_allow_html=True)
-    st.write("<div style='text-align: center;'>College of Engineering and Technology</div>", unsafe_allow_html=True)
-    st.markdown("<h6 style='text-align: center;'>Instrumentation and Electronics Engineering</h6>", unsafe_allow_html=True)
-    st.markdown("<div style='text-align: center;'>AISSCE (2012-2014)</div>", unsafe_allow_html=True)
-    st.write("<div style='text-align: center;'>Mother's Public School</div>", unsafe_allow_html=True)
-    st.markdown("<h6 style='text-align: center;'>CBSE</h6>", unsafe_allow_html=True)
+st.write("I am a technology geek interested in exploring ML and AI domain. With 4+ years into Data Science and 2+ years working in Azure Cloud, I am interested in exploring fields such as Natural Language Processing and Generative AI. I have experience in building models as well as deploying them over Docker or Cloud platforms")
 
 st.markdown("####")
 st.markdown("####")
-st.markdown("####")
 
-col_ed1, col_ed2, col_ed3 = st.columns(3)
+col_ed1, col_ed2, col_ed3, col_ed4 = st.columns(4)
 
 with col_ed1:
-    st.markdown("<h3 style='text-align: center;'>SKILLS</h3>", unsafe_allow_html=True)
-    col_skills1, col_skills2 = st.columns([0.5,0.5])
-    with col_skills1:
-        st.write("<div style='text-align: center;'>Python</div>", unsafe_allow_html=True)
-        st.write("<div style='text-align: center;'>Azure Cloud</div>", unsafe_allow_html=True)
-        st.write("<div style='text-align: center;'>Tensorflow/Keras/PyTorch</div>", unsafe_allow_html=True)
-        st.write("<div style='text-align: center;'>NLP</div>", unsafe_allow_html=True)
-        st.write("<div style='text-align: center;'>Docker</div>", unsafe_allow_html=True)
-    with col_skills2:
-        st.write("<div style='text-align: center;'>SQL</div>", unsafe_allow_html=True)
-        st.write("<div style='text-align: center;'>Tableau/Power BI</div>", unsafe_allow_html=True)
-        st.write("<div style='text-align: center;'>Flask/Streamlit</div>", unsafe_allow_html=True)
-        st.write("<div style='text-align: center;'>Unix/Linux</div>", unsafe_allow_html=True)
-        st.write("<div style='text-align: center;'>Git/GitHub</div>", unsafe_allow_html=True)
+    st.markdown("<h4>EDUCATION</h3>", unsafe_allow_html=True)
+    st.markdown("<div>B.Tech (2014-2018)</div>", unsafe_allow_html=True)
+    st.write("<div>College of Engineering and Technology</div>", unsafe_allow_html=True)
+    st.markdown("<h6>Instrumentation and Electronics Engineering</h6>", unsafe_allow_html=True)
+    st.markdown("<div>AISSCE (2012-2014)</div>", unsafe_allow_html=True)
+    st.write("<div>Mother's Public School</div>", unsafe_allow_html=True)
+    st.markdown("<h6>CBSE</h6>", unsafe_allow_html=True)
 
 with col_ed2:
-    st.markdown("<h3 style='text-align: center;'>CERTIFICATIONS</h3>", unsafe_allow_html=True)
-    st.markdown("")
-    st.write("<div style='text-align: center;'>AZ900: Azure Fundamentals</div>", unsafe_allow_html=True)
-    st.markdown("")
-    st.write("<div style='text-align: center;'>DP100: Azure Data Scientist Associate</div>", unsafe_allow_html=True)
+    st.markdown("<h4>SKILLS</h3>", unsafe_allow_html=True)
+    col_skills1, col_skills2 = st.columns([0.5,0.5])
+    with col_skills1:
+        st.write("<div>Python</div>", unsafe_allow_html=True)
+        st.write("<div>Azure Cloud</div>", unsafe_allow_html=True)
+        st.write("<div>Tensorflow/Keras</div>", unsafe_allow_html=True)
+        st.write("<div>PyTorch</div>", unsafe_allow_html=True)
+        st.write("<div>NLP</div>", unsafe_allow_html=True)
+        st.write("<div>Docker</div>", unsafe_allow_html=True)
+    with col_skills2:
+        st.write("<div>SQL</div>", unsafe_allow_html=True)
+        st.write("<div>Tableau/Power BI</div>", unsafe_allow_html=True)
+        st.write("<div>Flask</div>", unsafe_allow_html=True)
+        st.write("<div>Streamlit</div>", unsafe_allow_html=True)
+        st.write("<div>Unix/Linux</div>", unsafe_allow_html=True)
+        st.write("<div>Git/GitHub</div>", unsafe_allow_html=True)
 
 with col_ed3:
-    st.markdown("<h3 style='text-align: center;'>YEARS OF EXPERIENCE</h3>", unsafe_allow_html=True)
-    st.write("<div style='text-align: center;'>Total IT Experience: 5 yrs</div>", unsafe_allow_html=True)    
-    st.write("<div style='text-align: center;'>Python: 4.5 yrs</div>", unsafe_allow_html=True)
-    st.write("<div style='text-align: center;'>Data Science: 4 yrs</div>", unsafe_allow_html=True)
-    st.write("<div style='text-align: center;'>SQL: 4 yrs</div>", unsafe_allow_html=True)
-    st.write("<div style='text-align: center;'>Cloud(Azure, AWS): 2 yrs</div>", unsafe_allow_html=True)
+    st.markdown("<h4>CERTIFICATIONS</h3>", unsafe_allow_html=True)
+    st.markdown("")
+    st.write("<div>AZ900: Azure Fundamentals</div>", unsafe_allow_html=True)
+    st.markdown("")
+    st.write("<div>DP100: Azure Data Scientist Associate</div>", unsafe_allow_html=True)
 
-st.markdown("####")
-st.markdown("####")
-st.markdown("####")
+with col_ed4:
+    st.markdown("<h4>YEARS OF EXPERIENCE</h3>", unsafe_allow_html=True)
+    st.write("<div>Total IT Experience: 5 yrs</div>", unsafe_allow_html=True)    
+    st.write("<div>Python: 4.5 yrs</div>", unsafe_allow_html=True)
+    st.write("<div>Data Science: 4 yrs</div>", unsafe_allow_html=True)
+    st.write("<div>SQL: 4 yrs</div>", unsafe_allow_html=True)
+    st.write("<div>Cloud(Azure, AWS): 2 yrs</div>", unsafe_allow_html=True)
 
-st.markdown("<h2 style='text-align: center;'>SHORT INTRODUCTION</h2>", unsafe_allow_html=True)
-st.write("I am a technology geek interested in exploring ML and AI domain. With 4+ years into DataScience and 2+ years working in Azure Cloud, I am interested in exploring fields such as Natural Language Processing and Generative AI. I have experience in building models as well as deploying them over Docker or Cloud platforms")
-
-st.markdown("####")
-st.markdown("####")
-
-st.markdown("<h2 style='text-align: center;'>EXPERIENCE</h2>", unsafe_allow_html=True)
+st.markdown("<h2>EXPERIENCE</h2>", unsafe_allow_html=True)
 
 col_exp1, col_exp2 = st.columns(2)
 
 with col_exp1:
-    st.markdown("<h3 style='text-align: center;'>KPMG Global Services</h3>", unsafe_allow_html=True)
-    st.markdown("<h5 style='text-align: center;'>2022-Present</h5>", unsafe_allow_html=True)
+    st.markdown("<h4>KPMG Global Services</h3>", unsafe_allow_html=True)
+    st.markdown("<h5>2022-Present</h5>", unsafe_allow_html=True)
     st.markdown(
             """
-            - Worked for one of the biggest retail sector firm, developing a spam classification bot with language translation, embedded images etc. blockers
-            - Creation of raw dataset by email extraction from client mailbox using Python scripts, building various ML/DL models, deploying over Azure DevOps pipeline to Azure Web App and integration in Logic apps workflow
-            - Utilizing Azure tools such as Azure ML, Cognitive Services for same purpose
-            - Comparing all solutions by performance, efficiency and security aspects
-            - Reduction of manual workload by 83%
+            - Worked for one of the biggest retail sector firm, developing a spam classification bot
+            - Creation of raw dataset by email extraction from client mailbox using Python scripts
+            - Building various ML/DL models(Naive Bayes, XGBoost, LSTM) and creating Flask app
+            - Deploying Flask app over Azure DevOps pipeline to Azure Web App and integration in Logic apps workflow
+            - Implementing solutions to blockers such as non-English language emails using Language Translation, extracting text from embedded images using Image Captioning
+            - Utilizing Azure tools such as Azure Machine Learning, Cognitive Services for performance and speed improvement
+            - Implemented end-to-end project and deployed to Production including fixing issues during deployment, this solution reduces manual workload by 83%
             """
             )
-    st.markdown("####")
-    st.markdown("####")
     st.markdown("---")
     st.markdown(
             """
@@ -122,12 +122,14 @@ with col_exp1:
             )
 
 with col_exp2:
-    st.markdown("<h3 style='text-align: center;'>Tata Consultancy Services(TCS)</h3>", unsafe_allow_html=True)
-    st.markdown("<h6 style='text-align: center;'>2018-2022</h6>", unsafe_allow_html=True)
+    st.markdown("<h4>Tata Consultancy Services(TCS)</h3>", unsafe_allow_html=True)
+    st.markdown("<h5>2018-2022</h5>", unsafe_allow_html=True)
     st.markdown(
             """
-            - Worked for one of the biggest banks in Australia, developing a Document Image Processing and intelligent text extraction project, reducing human workload by completing real time work usually done in 3-5 business days to 30 minutes            - Creation of raw dataset by email extraction from client mailbox using Python scripts, building various ML/DL models, deploying over Azure DevOps pipeline to Azure Web App and integration in Logic apps workflow
-            - Classifying documents using state-of-the-art algorithms(88.89%), process and extract scanned/printed text using OCR, data and document comparison using fuzzy matching and regex algorithm to highlight key differences
+            - Worked for one of the biggest banks in Australia, developing a Document Image Processing and intelligent text extraction project, reducing human workload by completing real time work usually done in 3-5 business days to 30 minutes
+            - Classifying documents using state-of-the-art Image Classification algorithms like EfficientNet(88.89%), process and extract scanned/printed text using OCR
+            - Data and document comparison using fuzzy matching and regex algorithm to highlight key differences
+            - Extract text from scanned/searchable PDFs and images using python libraries
             - Extract information from structured/unstructured texts automatically, review the exceptions and recommend updates in the model for improved extraction
             - Deploying model into client's docker platform and integration in UI
             """
