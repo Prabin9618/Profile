8ac0a3b88fb12f4d80e626c6db3fdf7cd9619a2a
Prabin Dash <prabindash135@gmail.com>
2023-12-12 19:11:05 +0530

    Updated expander

--- profile.py
+++ profile.py
@@ -102,52 +102,52 @@ st.markdown("<h2>EXPERIENCE</h2>", unsafe_allow_html=True)
 col_exp1, col_exp2 = st.columns(2)
 
 with col_exp1:
-    st.markdown("<h4>KPMG Global Services</h3>", unsafe_allow_html=True)
-    st.markdown("<h5>2022-Present</h5>", unsafe_allow_html=True)
-    st.markdown(
-            """
-            - Worked for one of the biggest retail sector firm, developing a spam classification bot
-            - Creation of raw dataset by email extraction from client mailbox using Python scripts
-            - Building various ML/DL models(Naive Bayes, XGBoost, LSTM) and creating Flask app
-            - Deploying Flask app over Azure DevOps pipeline to Azure Web App and integration in Logic apps workflow
-            - Implementing solutions to blockers such as non-English language emails using Language Translation, extracting text from embedded images using Image Captioning
-            - Utilizing Azure tools such as Azure Machine Learning, Cognitive Services for performance and speed improvement
-            - Implemented end-to-end project and deployed to Production including fixing issues during deployment, this solution reduces manual workload by 83%
-            """
-            )
-    st.markdown("---")
-    st.markdown(
-            """
-            - Worked in Pharmaceutical sales domain to create Analytics Insights dashboard displaying multi-item(multiple orgs/products) Sales Forecasting and Anomaly Detection on Sales data over past timeperiod
-            - Extraction of raw data from SAP HANA database, copying to Azure Storage via Azure Data Factory, importing to Azure ML Studio and building models using various methods such as Automated ML
-            - Consuming model in PowerBI to create dashboard and setting up refresh
-            - Setting up AutoML feature to retrain ML model at certain frequency
-            """
-            )
+    with st.expander('KPMG Global Services'):
+        st.markdown("<h5>2022-Present</h5>", unsafe_allow_html=True)
+        st.markdown(
+                """
+                - Worked for one of the biggest retail sector firm, developing a spam classification bot
+                - Creation of raw dataset by email extraction from client mailbox using Python scripts
+                - Building various ML/DL models(Naive Bayes, XGBoost, LSTM) and creating Flask app
+                - Deploying Flask app over Azure DevOps pipeline to Azure Web App and integration in Logic apps workflow
+                - Implementing solutions to blockers such as non-English language emails using Language Translation, extracting text from embedded images using Image Captioning
+                - Utilizing Azure tools such as Azure Machine Learning, Cognitive Services for performance and speed improvement
+                - Implemented end-to-end project and deployed to Production including fixing issues during deployment, this solution reduces manual workload by 83%
+                """
+                )
+        st.markdown("---")
+        st.markdown(
+                """
+                - Worked in Pharmaceutical sales domain to create Analytics Insights dashboard displaying multi-item(multiple orgs/products) Sales Forecasting and Anomaly Detection on Sales data over past timeperiod
+                - Extraction of raw data from SAP HANA database, copying to Azure Storage via Azure Data Factory, importing to Azure ML Studio and building models using various methods such as Automated ML
+                - Consuming model in PowerBI to create dashboard and setting up refresh
+                - Setting up AutoML feature to retrain ML model at certain frequency
+                """
+                )
 
 with col_exp2:
-    st.markdown("<h4>Tata Consultancy Services(TCS)</h3>", unsafe_allow_html=True)
-    st.markdown("<h5>2018-2022</h5>", unsafe_allow_html=True)
-    st.markdown(
-            """
-            - Worked for one of the biggest banks in Australia, developing a Document Image Processing and intelligent text extraction project, reducing human workload by completing real time work usually done in 3-5 business days to 30 minutes
-            - Classifying documents using state-of-the-art Image Classification algorithms like EfficientNet(88.89%), process and extract scanned/printed text using OCR
-            - Data and document comparison using fuzzy matching and regex algorithm to highlight key differences
-            - Extract text from scanned/searchable PDFs and images using python libraries
-            - Extract information from structured/unstructured texts automatically, review the exceptions and recommend updates in the model for improved extraction
-            - Deploying model into client's docker platform and integration in UI
-            """
-            )
-    st.markdown("---")
-    st.markdown(
-            """
-            - Worked for one of the biggest banks in Australia, in Payments domain, utilizing skills such as Python, Oracle, Unix            
-            - Extracting huge chunks of data from Oracle database, analyzing them using Python Data Analysis tools, providing insights to clients
-            - Analyzing transactional processing data, implementing forecasting models to forecast weekly, monthly and quarterly expected loads, which helps reduce system downtime by 78%
-            - Trend Forecasting, Capacity report analysis, Dashboard preparation for monthly/quarterly processing and highlighting performance issues
-            - Developing cost/benefit analysis for business solutions
-            """
-            )
+    with st.expander('Tata Consultancy Services(TCS)'):
+        st.markdown("<h5>2018-2022</h5>", unsafe_allow_html=True)
+        st.markdown(
+                """
+                - Worked for one of the biggest banks in Australia, developing a Document Image Processing and intelligent text extraction project, reducing human workload by completing real time work usually done in 3-5 business days to 30 minutes
+                - Classifying documents using state-of-the-art Image Classification algorithms like EfficientNet(88.89%), process and extract scanned/printed text using OCR
+                - Data and document comparison using fuzzy matching and regex algorithm to highlight key differences
+                - Extract text from scanned/searchable PDFs and images using python libraries
+                - Extract information from structured/unstructured texts automatically, review the exceptions and recommend updates in the model for improved extraction
+                - Deploying model into client's docker platform and integration in UI
+                """
+                )
+        st.markdown("---")
+        st.markdown(
+                """
+                - Worked for one of the biggest banks in Australia, in Payments domain, utilizing skills such as Python, Oracle, Unix            
+                - Extracting huge chunks of data from Oracle database, analyzing them using Python Data Analysis tools, providing insights to clients
+                - Analyzing transactional processing data, implementing forecasting models to forecast weekly, monthly and quarterly expected loads, which helps reduce system downtime by 78%
+                - Trend Forecasting, Capacity report analysis, Dashboard preparation for monthly/quarterly processing and highlighting performance issues
+                - Developing cost/benefit analysis for business solutions
+                """
+                )
 
 st.markdown("<h2>PERSONAL PROJECTS</h2>", unsafe_allow_html=True)
 
